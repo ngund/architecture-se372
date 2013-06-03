cfae780ad223b8d03ca496069e5e505916d740bc
art.olkov <art.olkov@gmail.com>
2013-06-03 21:12:09 +0400

    Project created

---
+++ src/graph/graph.java
@@ -0,0 +1,11 @@
+package graph;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: mmaxy
+ * Date: 6/3/13
+ * Time: 9:10 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class graph {
+}
